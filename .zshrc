e67551c4c427ed3083a0c550a72aa62ed0359bfc
Rahul Gupta <112359655+rahul-lmg@users.noreply.github.com>
2023-12-20 06:57:26 +0000

    testing pulic calls

--- .zshrc
+++ .zshrc
@@ -113,5 +113,6 @@ export PATH="$PATH:$HOME/.rvm/bin"
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(zoxide init zsh)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,3 +2,7 @@ sudo apt update && apt install -y \
     software-properties-common && apt-add-repository -y \
     ppa:ansible/ansible && apt update && apt install -y \
     curl git ansible build-essential
+
+git clone https://github.com/rahul-lmg/setup.git
+cd setup
+ansibile-play local.yml
