ac7302ae25b4c8cd3a046a8d1be975f64b948354
Rahul Gupta <112359655+rahul-lmg@users.noreply.github.com>
2023-12-20 07:38:39 +0000

    clearn bash

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,8 +1,3 @@
-DEBIAN_FRONTEND=noninteractive apt update && apt install -y \
-    software-properties-common && apt-add-repository -y \
-    ppa:ansible/ansible && apt update && apt install -y \
-    curl git ansible build-essential -y
-
 git clone https://github.com/rahul-lmg/setup.git ~/setup
 cd ~/setup
-ansibile-play local.yml
+ansibile-playbook local.yml
